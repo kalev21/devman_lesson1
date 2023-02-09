f14aadb14f413fbba1ab9a423de608a6f541607a
Kalev21 <pfn21@bk.ru>
2023-02-09 19:42:36 +0300

    error correction

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 
-url_1 = 'https://wttr.in/%20Череповец?m?n?qTqu&lang=ru'
-url_2 = 'https://wttr.in/%20SVO?m?n?qTqu&lang=ru'
-url_3 = 'https://wttr.in/%20Лондон?m?n?qTqu&lang=ru'
-response_1 = requests.get(url_1)
-response_2 = requests.get(url_2)
-response_3 = requests.get(url_3)
-response_1.raise_for_status()
-print(response_1.text)
-print(response_2.text)
-print(response_3.text)
+urls = ('https://wttr.in/%20Череповец',
+        'https://wttr.in/%20SVO',
+        'https://wttr.in/%20Лондон')
+
+params = {'lang': 'ru', 'm': '', 'n': '', '': '%3F', 'q': '', 'T': '', 'u': ''}
+
+for url in urls:
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    print(response.text)
